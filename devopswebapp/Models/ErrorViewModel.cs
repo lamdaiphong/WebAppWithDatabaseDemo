9fcdc7377d294b1c1a655eeb0bb624541f41fb52
Jimmy Lam <lamdaiphong.nz@hotmail.com>
2022-08-25 00:57:14 +1200

    change app

--- devopswebapp/Models/ErrorViewModel.cs
+++ devopswebapp/Models/ErrorViewModel.cs
@@ -1,11 +1,8 @@
-using System;
+namespace devopswebapp.Models;
 
-namespace devopswebapp.Models
+public class ErrorViewModel
 {
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
+    public string? RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
