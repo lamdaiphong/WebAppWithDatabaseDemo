7c78cccf3a3d80fb9784d7d4c9d93ffcf0326326
Jimmy Lam <lamdaiphong.nz@hotmail.com>
2022-08-25 15:27:47 +1200

    fixed string and test

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot from Lam Dai Phong!";
+        return "Hello Winter from Lam Dai Phong!";
     }   
 }
